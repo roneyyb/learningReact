4135e3d015b3b577d019e8f653fe8d784d95b093
roneyyb <bothra.rajat08@gmail.com>
2020-08-06 12:57:42 +0530

    span list button div bootstrap

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counterComponent';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Counter/>,
   document.getElementById('root')
 );
 
